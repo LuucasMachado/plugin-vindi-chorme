0af903e35e06573eb77fcb36bbd479482534494b
lucasdourado <lucashenrique.2d@gmail.com>
2019-01-28 17:38:18 -0200

    iniciando

---
+++ inject/batbook.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+	$('.demo-logo img').hide();
+	$('.demo-logo').prepend('<img widht="117px" height="25px" src="https://www.vindi.com.br/assets/images/logo-footer.png" />')
+});
