ebf24219a7462616fc8673e2baf96f38b67e2bed
lucasdourado <lucashenrique.2d@gmail.com>
2019-02-01 13:34:24 -0200

    ajuste no plugin

--- inject/batbook.js
+++ inject/batbook.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
 	$('.demo-logo img').hide();
-	$('.demo-logo').prepend('<img widht="117px" height="25px" src="https://www.vindi.com.br/assets/images/logo-footer.png" />')
-});
+	$('.login-logo-vindi-image').hide();
+	$('.login-logo-vindi').prepend('<img widht="45px" height="45px" src="https://www.vindi.com.br/assets/images/logo-footer.png" />');
+	$('.demo-logo').prepend('<img widht="117px" height="25px" src="https://www.vindi.com.br/assets/images/logo-footer.png" />');
