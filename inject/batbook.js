57d0f26c0795493ab9417a438b07451df7de9c4f
Lucas Dourado <lucashenrique.2d@gmail.com>
2019-05-16 09:44:17 -0300

    remove

--- inject/batbook.js
+++ inject/batbook.js
@@ -1,4 +1,4 @@
-	$('.demo-logo img').hide();
+	
 	$('.login-logo-vindi-image').hide();
 	$('.login-logo-vindi').prepend('<img widht="45px" height="45px" src="https://www.vindi.com.br/assets/images/logo-footer.png" />');
 	$('.demo-logo').prepend('<img widht="117px" height="25px" src="https://www.vindi.com.br/assets/images/logo-footer.png" />');
